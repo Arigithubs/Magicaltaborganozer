eff17cde68104ac4860495ce5f74873df6113b08
Arigithubs <arimail@duck.com>
2023-12-26 12:58:51 -0500

    Refactor popup.js: Create, edit, and delete groups

--- popup.js
+++ popup.js
@@ -1,56 +1,35 @@
-// popup.js
-
-// Creating a new group
-function createGroup(groupName) {
-    chrome.storage.sync.get(['tabGroups'], function (result) {
-      const tabGroups = result.tabGroups || [];
-      const newGroup = { name: groupName, tabs: [] };
-      tabGroups.push(newGroup);
-      chrome.storage.sync.set({ tabGroups: tabGroups }, function () {
-        // Notify the interface or perform any additional actions upon group creation
-        console.log(`Group "${groupName}" created successfully.`);
-      });
-    });
-}
-
-// Editing a group
-function editGroup(oldName, newName) {
-    chrome.storage.sync.get(['tabGroups'], function (result) {
-      const tabGroups = result.tabGroups || [];
-      const groupToEdit = tabGroups.find(group => group.name === oldName);
-      if (groupToEdit) {
-        groupToEdit.name = newName;
-        chrome.storage.sync.set({ tabGroups: tabGroups }, function () {
-          // Notify the interface or perform any additional actions upon group editing
-          console.log(`Group "${oldName}" renamed to "${newName}".`);
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    // Initial load of groups
+    loadTabGroups(renderGroupList);
+  
+    // Event listener for the "Create Group" button
+    document.getElementById('createGroupBtn').addEventListener('click', function () {
+      const groupName = document.getElementById('groupNameInput').value.trim();
+      if (groupName !== '') {
+        createGroup(groupName);
+        loadTabGroups(renderGroupList);
+        document.getElementById('groupNameInput').value = '';
       }
     });
-}
-
-// Deleting a group
-function deleteGroup(groupName) {
-    chrome.storage.sync.get(['tabGroups'], function (result) {
-      const tabGroups = result.tabGroups || [];
-      const updatedGroups = tabGroups.filter(group => group.name !== groupName);
-      chrome.storage.sync.set({ tabGroups: updatedGroups }, function () {
-        // Notify the interface or perform any additional actions upon group deletion
-        console.log(`Group "${groupName}" deleted successfully.`);
-      });
+  
+    // Event listener for clicking on a group
+    document.getElementById('groupList').addEventListener('click', function (event) {
+      if (event.target.tagName === 'LI') {
+        const groupName = event.target.textContent.trim();
+        // Implement additional functionality when clicking on a group
+        console.log(`Clicked on group: ${groupName}`);
+      }
     });
-}
-
-// Saving tab groups to Chrome storage
-function saveTabGroups(tabGroups) {
-    chrome.storage.sync.set({ tabGroups: tabGroups }, function () {
-      console.log('Tab groups saved successfully.');
+  });
+  
+  // Function to render the list of groups
+  function renderGroupList(tabGroups) {
+    const groupList = document.getElementById('groupList');
+    groupList.innerHTML = '';
+    tabGroups.forEach(group => {
+      const listItem = document.createElement('li');
+      listItem.textContent = group.name;
+      groupList.appendChild(listItem);
     });
-}
-
-// Loading tab groups from Chrome storage
-function loadTabGroups(callback) {
-    chrome.storage.sync.get(['tabGroups'], function (result) {
-      const tabGroups = result.tabGroups || [];
-      callback(tabGroups);
-    });
-}
+  }
+  
